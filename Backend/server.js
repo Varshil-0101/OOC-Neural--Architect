5bc489256ff440bd5d05362ff6363c90f164d21e
varshil-0101 <varshilpatel213@gmail.com>
2026-03-01 17:07:33 +0530

    Fixed port for Render

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ import aiRoutes from "./routes/ai.routes.js";
 import priorityRoutes from "./routes/priority.routes.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -20,5 +20,5 @@ app.get("/", (_, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
+  console.log(`Backend running on port ${PORT}`);
 });
